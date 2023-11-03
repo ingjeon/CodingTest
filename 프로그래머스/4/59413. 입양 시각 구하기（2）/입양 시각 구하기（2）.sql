096a0cc450f7376726af1cfb527ef77cc4a2778c
ingjeon <141722181+ingjeon@users.noreply.github.com>
2023-11-03 17:36:47 +0900

    [level 4] Title: 입양 시각 구하기(2), Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/4/59413. 입양 시각 구하기（2）/입양 시각 구하기（2）.sql
+++ 프로그래머스/4/59413. 입양 시각 구하기（2）/입양 시각 구하기（2）.sql
@@ -1,2 +1,26 @@
-SELECT HOUR(DATETIME) AS HOUR, COUNT(ANIMAL_ID) 
-FROM ANIMAL_OUTS GROUP BY HOUR order by hour;
+-- 코드를 입력하세요
+# 0~23 시간대별 입양 건수 
+# HOUR(기준 날짜);
+# SET sql_mode = '';
+
+### n속성에 0~23인 cte 테이블 생성
+WITH RECURSIVE cte AS (
+         SELECT 0 AS n
+         UNION ALL
+         SELECT n + 1 FROM cte WHERE n < 23
+	)
+### 
+SELECT n, IFNULL(COUNT(ANIMAL_ID),0) 
+FROM cte
+LEFT JOIN ANIMAL_OUTS 
+ON cte.n =  DATE_FORMAT(DATETIME,'%H')
+GROUP BY n;
+
+# SELECT cte.n, IFNULL(A.total,0)
+# FROM (SELECT DATE_FORMAT(DATETIME,'%H') AS "COL_01",
+#     COUNT(ANIMAL_ID) AS total
+# 	FROM ANIMAL_OUTS
+# 	 GROUP BY DATE_FORMAT(DATETIME,'%H')
+# 	) A
+# RIGHT JOIN cte 
+# ON A.COL_01 = cte.n;
