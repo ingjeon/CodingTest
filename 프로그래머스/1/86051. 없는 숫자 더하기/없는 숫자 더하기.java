734d82f60c5d48b280ed3279e7a2b93fe9f7bb33
ingjeon <141722181+ingjeon@users.noreply.github.com>
2023-12-05 10:51:43 +0900

    [level 1] Title: 없는 숫자 더하기, Time: 0.02 ms, Memory: 73.8 MB -BaekjoonHub

--- 프로그래머스/1/86051. 없는 숫자 더하기/없는 숫자 더하기.java
+++ 프로그래머스/1/86051. 없는 숫자 더하기/없는 숫자 더하기.java
@@ -2,7 +2,7 @@ class Solution {
     public int solution(int[] numbers) {
         int answer = 45;
         for (int i = 0; i < numbers.length; i++){
-            if (1 <= numbers[i] && numbers[i] <=9) answer = answer - numbers[i];}
+            answer = answer - numbers[i];}
         return answer;
     }
 }
